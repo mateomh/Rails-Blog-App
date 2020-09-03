ade29c8345fb781d6157a08fe90b2e762168d3d4
mateomh <mmojicah.84@gmail.com>
2020-09-03 12:45:50 -0500

    Create the destroy action in the articles controller and update the show action view to ask for confirmation on the deletion of the data

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,4 +21,10 @@ class ArticlesController < ApplicationController
     @article.save
     redirect_to article_path(@article)
   end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to article_path
+  end
 end
