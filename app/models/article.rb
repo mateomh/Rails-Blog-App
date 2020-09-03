2e19c6af182ba46722ab837fd34765fbae504bdd
mateomh <mmojicah.84@gmail.com>
2020-09-03 15:51:28 -0500

    Create the relationship between the tags articles and taggins models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ApplicationRecord
   has_many :comments
+  has_many :taggins
+  has_many :tags through :taggins
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,4 @@
 class Tag < ApplicationRecord
+  has many :taggins
+  has_many :articles through :taggins
 end
