bef1769d7829a02c302041da4e61c769dacfb2d7
mateomh <mmojicah.84@gmail.com>
2020-09-03 14:20:52 -0500

    Create the update action for the articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -31,4 +31,11 @@ class ArticlesController < ApplicationController
   def edit
     @article = Article.find(params[:id])
   end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    redirect_to article_path(@article)
+  end
 end
