b08a93df52d344da2f8963ba978a4fd76b62c4de
mateomh <mmojicah.84@gmail.com>
2020-09-03 15:58:45 -0500

    Add tag list method to the article class and made the tag list a permited parameter in the articles helper

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,10 @@ class Article < ApplicationRecord
   has_many :comments
   has_many :taggins
   has_many :tags through :taggins
+  
+  def tag_list
+    self.tags.collect do |tag|
+      tag.name
+    end.join(", ")
+  end
 end
